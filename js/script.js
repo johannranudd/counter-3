028a9a960c07342cd366fe0e81e99c0d0de06356
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-10 21:14:07 +0200

    sdv

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const time = new Date().getTime();
+const day = new Date().getDay();
+const hour = new Date().getHours();
+const minute = new Date().getMinutes();
+const seconds = new Date().getSeconds();
