929829284d6b26d9e918455c7bc7356840d39865
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-10 22:14:23 +0200

    seg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const time = new Date().getTime();
-const day = new Date().getDay();
-const hour = new Date().getHours();
-const minute = new Date().getMinutes();
-const seconds = new Date().getSeconds();
+const daysDiv = document.querySelector(".days h2");
+const secondsDiv = document.querySelector(".seconds h2");
+
+function getTimeAndDate() {
+  const now = new Date();
+  const day = new Date().getDay();
+  const hour = new Date().getHours();
+  const minute = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+
+  const countDownDate = new Date(now.getTime() + 1);
+  console.log(countDownDate);
+
+  //   console.log(countDownDate);
+
+  //   const giveawayDate = now + 10;
+  //   console.log(giveawayDate);seconds;
+}
+
+setInterval(function () {
+  getTimeAndDate();
+}, 1000);
